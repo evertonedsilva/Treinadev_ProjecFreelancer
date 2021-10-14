bbd4a9740542cc434da8d5dab3c8dc68dc581c97
Everton Silva <everton.edesio@gmail.com>
2021-10-14 10:44:15 -0300

    Perfil freelancer corrigido

--- app/controllers/freelancers_controller.rb
+++ app/controllers/freelancers_controller.rb
@@ -1,6 +1,4 @@
 class FreelancersController < ApplicationController
   before_action :authenticate_freelancer!, only: [:new, :create, :update]
-  def lancer_profile
-    @freelancer = current_freelancer
-  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :freelancers
   devise_for :employers
-  root to: 'home#index'
-  get 'lancer_profile', to: 'freelancers#lancer_profile'  
+  root to: 'home#index' 
   resources :lancer_infos, only: [:index, :show, :new, :create, :update]
 end
